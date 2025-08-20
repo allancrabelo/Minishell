223e7780e0c5c13de41a8a2c97b5a44b5f957d27
allancrabelo <allanaugustocarneirorabelo@gmail.com>
2025-08-20 17:30:48 +0100

    added: library includes.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,11 +6,27 @@
 /*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 17:05:28 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/08/20 17:05:59 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/08/20 17:30:04 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+#include <stdio.h>				// printf, perror
+#include <stdlib.h>				// malloc, free, exit
+#include <unistd.h>				// write, access, read, close, fork, chdir, unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot, getcwd
+#include <fcntl.h>				// open
+#include <sys/stat.h>			// stat, lstat, fstat
+#include <sys/wait.h>			// wait, waitpid, wait3, wait4
+#include <signal.h>				// signal, sigaction, sigemptyset, sigaddset, kill
+#include <dirent.h>				// opendir, readdir, closedir
+#include <string.h>				// strerror
+#include <errno.h>				// errno
+#include <termios.h>			// tcsetattr, tcgetattr
+#include <sys/ioctl.h>			// ioctl
+#include <curses.h>				// tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs (ou <termcap.h>)
+#include <readline/readline.h>	// readline
+#include <readline/history.h>	// add_history, rl_clear_history, etc.
 
 #endif
