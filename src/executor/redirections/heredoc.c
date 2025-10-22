816bd8380ced3b354ef51e0762f16bd582c3e7a6
Miguel Azuaga <miguel.q.azuaga@gmail.com>
2025-10-22 15:51:40 +0100

    Refactor heredoc with pipes

--- src/executor/redirections/heredoc.c
+++ src/executor/redirections/heredoc.c
@@ -1,5 +1,39 @@
 #include "../../../includes/minishell.h"
 
+static int	is_delimiter(char *line, char *delimiter);
+static char	*expand_heredoc_line(t_mini *mini, char *line);
+
+static void	write_heredoc_to_pipe(int write_fd, char *delimiter, t_mini *mini)
+{
+	char	*line;
+	char	*expanded_line;
+
+	while (1)
+	{
+		line = readline("> ");
+		if (!line && g_signal != 130)
+		{
+			ft_putstr_fd("minishell: warning: here-document \
+delimited by end-of-file (wanted `", 2);
+			ft_putstr_fd(delimiter, 2);
+			ft_putstr_fd("')\n", 2);
+			break ;
+		}
+		if (is_delimiter(line, delimiter))
+			break ;
+		expanded_line = expand_heredoc_line(mini, line);
+		if (expanded_line)
+		{
+			ft_putendl_fd(expanded_line, write_fd);
+			free(expanded_line);
+		}
+		else
+			ft_putendl_fd(line, write_fd);
+		free(line);
+	}
+	close(write_fd);
+}
+
 static char	*extract_and_expand_var(t_mini *mini, char *line, int *i)
 {
 	char	*var_name;
@@ -102,20 +136,6 @@ static void	heredoc_append(t_heredoc **heredoc, t_heredoc *new)
 	}
 }
 
-static char	*get_filename(int *heredoc_fd)
-{
-	char	*filename;
-	char	*heredoc_string;
-
-	heredoc_string = ft_itoa(*heredoc_fd);
-	if (!heredoc_string)
-		return (NULL);
-	filename = ft_strjoin(".heredoc_", heredoc_string);
-	free(heredoc_string);
-	(*heredoc_fd)++;
-	return (filename);
-}
-
 static int	is_delimiter(char *line, char *delimiter)
 {
 	if (ft_strlen(line) == ft_strlen(delimiter)
@@ -127,38 +147,9 @@ static int	is_delimiter(char *line, char *delimiter)
 	return (0);
 }
 
-static void	write_heredoc(int fd, char *delimiter, int interrupted, t_mini *mini)
-{
-	char	*line;
-	char	*expanded_line;
+// This function is now integrated into write_heredoc_to_pipe
 
-	(void)interrupted;
-	while (1)
-	{
-		line = readline("> ");
-		if (!line)
-		{
-			ft_putstr_fd("minishell: warning: here-document \
-delimited by end-of-file (wanted `", 2);
-			ft_putstr_fd(delimiter, 2);
-			ft_putstr_fd("')\n", 2);
-			break ;
-		}
-		if (is_delimiter(line, delimiter))
-			break ;
-		expanded_line = expand_heredoc_line(mini, line);
-		if (expanded_line)
-		{
-			ft_putendl_fd(expanded_line, fd);
-			free(expanded_line);
-		}
-		else
-			ft_putendl_fd(line, fd);
-		free(line);
-	}
-}
-
-void	heredoc_cleaner(t_heredoc **heredoc, int unlinker)
+void	heredoc_cleaner(t_heredoc **heredoc)
 {
 	t_heredoc	*cur;
 	t_heredoc	*tmp;
@@ -169,12 +160,8 @@ void	heredoc_cleaner(t_heredoc **heredoc, int unlinker)
 		tmp = cur->next;
 		if (cur->heredoc_delimeter)
 			free(cur->heredoc_delimeter);
-		if (cur->filename)
-		{
-			if (unlinker)
-				unlink(cur->filename);
-			free(cur->filename);
-		}
+		if (cur->pipe_fd > 0)
+			close(cur->pipe_fd);
 		free(cur);
 		cur = tmp;
 	}
@@ -198,41 +185,41 @@ static void	wait_heredoc(t_mini *mini, pid_t pid)
 	}
 }
 
-static void	read_heredoc(t_mini *mini, char *filename, char *delimiter)
+static int	read_heredoc_with_pipe(t_mini *mini, char *delimiter)
 {
-	int	fd;
+	int		pipe_fd[2];
 	pid_t	pid;
-	int	interrupted;
 
-	interrupted = 0;
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
+	if (pipe(pipe_fd) == -1)
 	{
-		print_command_error(filename, strerror(errno));
-		return ;
+		perror("pipe");
+		return (-1);
 	}
 	signal(SIGINT, SIG_IGN);
 	pid = fork();
 	if (pid < 0)
 	{
-		close(fd);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
 		ft_free_all(mini, errno, 1);
 	}
 	else if (pid == 0)
 	{
+		close(pipe_fd[0]);
 		setup_heredoc_signals();
-		write_heredoc(fd, delimiter, interrupted, mini);
-		close(fd);
-		exit(g_signal);
+		write_heredoc_to_pipe(pipe_fd[1], delimiter, mini);
+		ft_free_all(mini, g_signal, 1);
 	}
-	close(fd);
+	close(pipe_fd[1]);
 	wait_heredoc(mini, pid);
 	signal_init();
+	return (pipe_fd[0]);
 }
 
-void	create_heredoc_file(t_mini *mini, char *delimiter, t_heredoc **heredoc)
+void	create_heredoc_pipe(t_mini *mini, char *delimiter, t_heredoc **heredoc)
 {
 	t_heredoc	*new;
+	int			pipe_fd;
 
 	new = malloc(sizeof(t_heredoc));
 	if (!new)
@@ -244,39 +231,33 @@ void	create_heredoc_file(t_mini *mini, char *delimiter, t_heredoc **heredoc)
 		free(new);
 		ft_free_all(mini, ENOMEM, 1);
 	}
-	new->filename = get_filename(&mini->heredoc_fd);
-	if (!new->filename)
+	pipe_fd = read_heredoc_with_pipe(mini, delimiter);
+	if (pipe_fd == -1)
 	{
 		free(new->heredoc_delimeter);
 		free(new);
 		ft_free_all(mini, ENOMEM, 1);
 	}
+	new->pipe_fd = pipe_fd;
 	heredoc_append(heredoc, new);
-	read_heredoc(mini, new->filename, new->heredoc_delimeter);
 }
 
 int	redirect_heredoc(t_redir *redirect, t_mini *mini)
 {
-	int	fd;
-
 	(void)redirect;
-	if (!mini->heredoc || !mini->heredoc->filename)
+	if (!mini->heredoc || mini->heredoc->pipe_fd < 0)
 	{
-		ft_putstr_fd("minishell: heredoc: file not found\n", 2);
+		print_command_error("heredoc", "pipe not available");
 		return (-1);
 	}
-	fd = open(mini->heredoc->filename, O_RDONLY);
-	if (fd == -1)
+	if (dup2(mini->heredoc->pipe_fd, STDIN_FILENO) == -1)
 	{
-		print_command_error(mini->heredoc->filename, strerror(errno));
+		perror("dup2");
 		return (-1);
 	}
-	if (dup2(fd, STDIN_FILENO) == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
+	/* Close the original pipe fd since it's now duplicated to stdin */
+	close(mini->heredoc->pipe_fd);
+	mini->heredoc->pipe_fd = -1;
 	return (0);
 }
 
@@ -293,7 +274,7 @@ void	process_heredocs(t_mini *mini, t_ast *node)
 		{
 			if (redir->type == TOKEN_HEREDOC)
 			{
-				create_heredoc_file(mini, redir->heredoc_delimeter,
+				create_heredoc_pipe(mini, redir->heredoc_delimeter,
 					&mini->heredoc);
 				if (mini->heredoc_signal)
 					return ;
